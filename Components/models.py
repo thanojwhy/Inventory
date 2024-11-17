a1655c5cbcdfe4cb650f3078c97576486efd844f
thanojwhy <thanoj1992@gmail.com>
2024-11-17 21:02:36 +0530

    display components

--- Components/models.py
+++ Components/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Component(models.Model):
+    name=models.CharField(max_length=50)
+    category=models.CharField(max_length=10)
+    description=models.TextField()
+    unitMeasure=models.CharField(max_length=8)
+    costPerUnit=models.IntegerField()
+    currentStock=models.PositiveIntegerField()
+    maximumStock=models.PositiveIntegerField()
+    safeStock=models.PositiveIntegerField()
+    industryLine=models.CharField(max_length=15)
+    suppliedBy=models.CharField(max_length=25)
+
+    def __str__(self):
+        return f"{self.name}"
--- Components/views.py
+++ Components/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Component
 # Create your views here.
 
 def component(request):
+    complist=Component.objects.all().values()
     template=loader.get_template('compList.html')
     context={
-        "name":""
+        "components":complist
     }
     return HttpResponse(template.render(context,request))
 
