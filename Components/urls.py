8c20d64ddcbca5484b66184379e20b4ee9b8a176
thanojwhy <thanoj1992@gmail.com>
2024-11-21 18:38:08 +0530

    Component Update functionality added

--- Components/urls.py
+++ Components/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name="components"
 
 urlpatterns = [
+    path('update-usage/<int:c_id>',views.update_usage,name="update"),
     path('list/',views.component,name='list'),
     path('usage/',views.usage,name="usage"),
 ]
--- Components/views.py
+++ Components/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404,redirect
 from django.template import loader
 from .models import Component
 # Create your views here.
@@ -10,6 +11,29 @@ def component(request):
         "components":complist
     }
     return HttpResponse(template.render(context,request))
+
+def update_usage(request,c_id):
+    if request.method=="POST":
+        c=get_object_or_404(Component,id=c_id)
+        try:
+            usage_update=int(request.POST.get('usage_update'))
+
+            if usage_update>c.currentStock:
+                return render(request,'compList.html',{
+                    'c':Component.objects.all(),
+                    'error':f"Insufficient stock for {c.name}",
+                })
+            c.currentStock-=usage_update
+            c.save()
+
+            return redirect('components:list')
+        except (ValueError,KeyError):
+            return render(request,'compList.html',{
+                'c':Component.objects.all(),
+                'error':"Invalid input",
+            })
+    else:
+        return redirect('components:list')
 
 def usage(request):
     template=loader.get_template('usage.html')
